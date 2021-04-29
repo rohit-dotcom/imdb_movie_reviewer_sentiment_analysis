a763356d43fe4236f19c27a39e8dd0733704b7bb
Rohit Sharma <rohit.enggboy@gmail.com>
2021-04-29 08:20:00 +0530

    Inserting

--- app_sentiment_analysis/flask_web_api.py
+++ app_sentiment_analysis/flask_web_api.py
@@ -1,15 +1,70 @@
 from flask import (Flask, render_template, abort,url_for,
                    jsonify, request, redirect)
+import re
+import tensorflow as tf
+import keras
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+import os
+import json
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 app=Flask(__name__)
+
+
+def model_loader(modelpath):
+    weights_path = modelpath + '/weights/'
+    config_file = os.path.join(modelpath, 'model_config.txt')
+    with open(config_file) as f:
+        json_config = json.load(f)
+    print(json_config)
+    model_arch = tf.keras.Sequential.from_config(json_config)
+    model_arch.summary()
+    model_arch.load_weights(weights_path)
+
+    return model_arch
+
+model=model_loader('./lstm_model')
 
 @app.route("/", methods=["GET","POST"])
 def welcome():
     if request.method == "POST":
         review = request.form['review']
-        return render_template("welcome.html")
+        review = prepare_review(review)
+        print(review)
+        input_ready_review = create_OHE_padded_docs([review], 100000, 250)
+        print('sending for prediction')
+        prediction = model.predict(input_ready_review)
+        print(prediction)
+        sentiment = 'Positive' if prediction[0][0]>0.5 else 'Negative'
+        return render_template("welcome.html",result=sentiment)
     else:
         return render_template("welcome.html")
+
+
+def create_OHE_padded_docs(text_doc, vocab_size, max_length):
+    print('pre processing started')
+    embedded_docs = [one_hot(i, n=vocab_size) for i in text_doc]
+    padded_embedded_docs = pad_sequences(embedded_docs, maxlen=max_length, padding='post', truncating='post')
+    print('preprocesing complted')
+    return padded_embedded_docs
+
+
+def prepare_review(review):
+    only_words=re.findall('[a-z0-9]+',review,re.IGNORECASE)
+    only_text=' '.join(only_words)
+    return only_text
 
 
 if __name__ =='__main__':
--- app_sentiment_analysis/model.py
+++ app_sentiment_analysis/model.py
@@ -1,37 +1,15 @@
 import numpy as np
-import re
+import pandas as pd
 import os
 from tensorflow import keras
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-import random
+import json
+import sys
+import shutil
 
-pos_train_path = 'C:/Users/91911/Documents/Python Scripts/Data/Imdb/aclImdb/train/pos/'
-neg_train_path = 'C:/Users/91911/Documents/Python Scripts/Data/Imdb/aclImdb/train/neg/'
-len(os.listdir(pos_train_path))
-
-def get_data_with_label(path, positive=True):
-    """set positive as False if you want to get negative reviews"""
-    label = 1 if positive else 0
-    reviews = []
-    for data_file in os.listdir(path):
-        with open(path + data_file, 'rb') as reader:
-            only_words = re.findall('[a-z0-9]+', reader.readline().decode(), re.IGNORECASE)
-            only_text = ' '.join(only_words)
-            reviews.append((only_text, label))
-    return reviews
-
-positive_reviews = get_data_with_label(pos_train_path)
-
-negative_reviews = get_data_with_label(neg_train_path, positive=False)
-
-pos_and_neg_reviews = positive_reviews + negative_reviews
-
-random.shuffle(pos_and_neg_reviews)
-reviews = [i for i, j in pos_and_neg_reviews]
-
-labels = [j for i, j in pos_and_neg_reviews]
+dataframe = pd.read_csv('./Data/reviews_dataset.csv')
 
 
 def create_OHE_padded_docs(text_doc, vocab_size, max_length):
@@ -54,32 +32,46 @@ def build_LSTM_model(vocab_size, max_length, n_features):
     return model
 
 
-LSTM_ready_input_text_doc = create_OHE_padded_docs(reviews, 100000, 250)
+LSTM_ready_input_text_doc = create_OHE_padded_docs(dataframe['review_text'], 100000, 250)
 
 vocab_size = 100000
 max_length = 250
 batch_size = 25
-num_epochs = 20
+num_epochs = 7
 num_features = 50
-
 
 lstm_model = build_LSTM_model(vocab_size, max_length, n_features=num_features)
 
-
-lstm_model.fit(LSTM_ready_input_text_doc, np.array(labels), batch_size=batch_size,
+lstm_model.fit(LSTM_ready_input_text_doc, np.array(dataframe['label'].values), batch_size=batch_size,
                epochs=num_epochs)
+'''
+initial attempt to save the model--failed
 
 filename = 'lstm_imdb_moview_model'
-
-
 def save_model(model, filename):
     keras.models.save_model(model, filename)
-
-
 save_model(lstm_model, filename)
+'''
 
 
+def save_model(model, absolute_filepath):
+    try:
+        os.mkdir(absolute_filepath)
+    except Exception as e:
+        print(e)
+        resp = input("Do you want us remove the previous file and create new(y/n): ")
+        if resp == 'y':
+            shutil.rmtree(absolute_filepath)
+            os.mkdir(absolute_filepath)
+        else:
+            print('Not saving the model')
+            sys.exit()
+    weights_path = os.path.join(absolute_filepath, 'weights')
+    os.mkdir(weights_path)
+    json_config = model.get_config()
+    with open(os.path.join(absolute_filepath, 'model_config.txt'), 'w') as wrtfile:
+        json.dump(json_config, wrtfile)
+    model.save_weights(weights_path + "/")
 
 
-
-
+save_model(lstm_model, './lstm_model')
